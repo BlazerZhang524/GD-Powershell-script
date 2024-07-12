7f53949d0968aa91104bcb3d349060723a882ffd
BlazerZhang524 <148676451+BlazerZhang524@users.noreply.github.com>
2024-07-12 12:35:39 +0800

    Add files via upload

---
+++ Remove registry key for reenroll intune..ps1
@@ -0,0 +1,10 @@
+﻿Write-Host Copy Intune Enroll ID from Task scheduler\windows\EnterpriseMgmt
+$IntuneEnrollID=Read-Host
+Get-Item HKLM:\SOFTWARE\Microsoft\Enrollments\$IntuneEnrollID|Remove-Item -Force -Verbose
+Get-Item HKLM:\SOFTWARE\Microsoft\Enrollments\Status\$IntuneEnrollID|Remove-Item -Force -Verbose
+Get-Item HKLM:\SOFTWARE\Microsoft\EnterpriseResourceManager\Tracked\$IntuneEnrollID|Remove-Item -Force -Verbose
+Get-Item HKLM:\SOFTWARE\Microsoft\PolicyManager\AdmxInstalled\$IntuneEnrollID|Remove-Item -Force -Verbose
+Get-Item HKLM:\SOFTWARE\Microsoft\PolicyManager\Providers\$IntuneEnrollID|Remove-Item -Force -Verbose
+Get-Item HKLM:\SOFTWARE\Microsoft\Provisioning\OMADM\Accounts\$IntuneEnrollID|Remove-Item -Force -Verbose
+Get-Item HKLM:\SOFTWARE\Microsoft\Provisioning\OMADM\Logger\$IntuneEnrollID|Remove-Item -Force -Verbose
+Get-Item HKLM:\SOFTWARE\Microsoft\Provisioning\OMADM\Sessions\$IntuneEnrollID|Remove-Item -Force -Verbose
