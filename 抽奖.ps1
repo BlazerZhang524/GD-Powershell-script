5d86fae372e8ce6a73fc9fc365470e7cc3149a3f
BlazerZhang524 <148676451+BlazerZhang524@users.noreply.github.com>
2024-07-12 12:57:19 +0800

    Add files via upload

---
+++ 抽奖.ps1
@@ -0,0 +1,6 @@
+﻿$user = Import-Csv C:\temp\user.csv
+$lukcyone=Get-Random $user
+Write-Host $lukcyone -ForegroundColor Green
+$user = [System.Collections.ArrayList]$user
+$user.Remove($lukcyone) 
+$user | Export-Csv C:\temp\user.csv -NoTypeInformation
